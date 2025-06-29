568692e53c810a80efb8371fc351b85d57dcffb1
Michail Tatas <michail.tatas@gmail.com>
2025-06-30 00:10:53 +0300

    can now print to VGA | most of output functionality implemented

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-
 // Function called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -15,14 +13,10 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use core::fmt::Write;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop{}
 }
