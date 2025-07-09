0ba1168f18ea22329f35362011543820c0234f21
Michail Tatas <michail.tatas@gmail.com>
2025-07-10 01:35:43 +0300

    more detailed messages in the exception handler

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -5,13 +5,39 @@ mod idt;
 lazy_static! {
     static ref IDT: idt::Idt = {
         let mut idt = idt::Idt::new();
-        idt.set_handler(0, divide_by_zero_handler);
+        idt.set_handler(0, divide_by_zero_wrapper);
 
         idt
     };
 }
 
-extern "C" fn divide_by_zero_handler() -> ! {
+#[derive(Debug)]
+#[repr(C)]
+struct ExceptionStackFrame {
+    instruction_pointer: u64,
+    code_segment: u64,
+    cpu_flags: u64,
+    stack_pointer: u64,
+    stack_segment: u64,
+}
+
+// use naked function wrapper because otherwise
+// we have the prologue of the function first 
+// and reading rsp gives us junk
+#[unsafe(naked)]
+extern "C" fn divide_by_zero_wrapper() -> ! {
+    unsafe {
+        core::arch::naked_asm!(
+            "mov rdi, rsp",                // Move stack pointer to rdi
+            "call {handler}",              // Call Rust handler function
+            handler = sym divide_by_zero_handler,
+        )
+    }
+}
+
+extern "C" fn divide_by_zero_handler(stack_frame: ExceptionStackFrame) -> ! {
+    println!("{:#?}", stack_frame);
+
     println!("EXCEPTION: DIVIDE BY ZERO !!");
 
     loop{}
